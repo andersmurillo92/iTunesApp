7099012803cc18924a873f7e4e8463e0fde872d2
John Anders Murillo <47459887+andersmurillo92@users.noreply.github.com>
2021-02-19 09:55:41 -0500

    Create and implement favorite activity

---
+++ app/src/main/java/com/janders/itunesapp/views/favorites/FavoriteActivity.kt
@@ -0,0 +1,14 @@
+package com.janders.itunesapp.views.favorites
+
+import android.os.Bundle
+import com.janders.itunesapp.R
+import com.janders.itunesapp.views.base.BaseActivity
+
+class FavoriteActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+
+}
